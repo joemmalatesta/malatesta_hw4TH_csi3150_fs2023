48ef2f4cfb98242ba8d87828016c0dee9891399e
Joe Malatesta <joemmalatesta@gmail.com>
2023-10-28 01:10:59 -0700

    project completely done. Selecting by make is so dumb..

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ const maxPriceInput = document.getElementById("maxPrice");
 const colorFilterCheckboxes = document.querySelectorAll(
   'input[name="colorFilter"]'
 );
+const makeFilterCheckboxes = document.querySelectorAll('input[name="make"]');
 
 function filterCars() {
   let minYear = minYearInput.value || 0;
@@ -39,6 +40,9 @@ function filterCars() {
   let maxPrice = maxPriceInput.value || Infinity;
   // Collect the selected colors from the checkboxes
   const selectedColors = Array.from(colorFilterCheckboxes)
+    .filter((checkbox) => checkbox.checked)
+    .map((checkbox) => checkbox.value);
+  const selectedMake = Array.from(makeFilterCheckboxes)
     .filter((checkbox) => checkbox.checked)
     .map((checkbox) => checkbox.value);
 
@@ -55,7 +59,8 @@ function filterCars() {
       car.year >= minYear &&
       car.year <= maxYear &&
       car.price <= maxPrice &&
-      (selectedColors.length === 0 || selectedColors.includes(car.color))
+      (selectedColors.length === 0 || selectedColors.includes(car.color) ) &&
+      (selectedMake.length === 0 || selectedMake.includes(car.make))
     ) {
       const carComponent = createCarComponent(car);
       carListContainer.appendChild(carComponent);
@@ -70,12 +75,10 @@ function filterCars() {
   }
 }
 
-
-
 // Reset button logic
 function resetFilters() {
   // Reset all filters
-  colorFilterCheckboxes.forEach(checkbox => checkbox.checked = false);
+  colorFilterCheckboxes.forEach((checkbox) => (checkbox.checked = false));
   minYearInput.value = "";
   maxYearInput.value = "";
   maxMilageInput.value = "";
@@ -85,11 +88,10 @@ function resetFilters() {
   filterCars();
 }
 
-
 // DOM INTERACTION
 // Reset event listener
 resetButton.addEventListener("click", resetFilters);
 // Filter Event listener
 filterForm.addEventListener("click", filterCars);
 // Just the first time.. Show everything.
-filterCars()
+filterCars();
